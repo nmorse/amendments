5415dbda1be211f69ceb87943046f89748b0289a
nmorse <morse.nate@gmail.com>
2019-09-16 20:47:51 -0400

    check for events matching next events

--- src/Amendment.ezui.js
+++ src/Amendment.ezui.js
@@ -2,25 +2,12 @@ import React from 'react';
 import { useMachine } from '@xstate/react';
 import { amendmentMachine } from './AmendmentMachine';
 
-export function HandlesEvent({ children, event, current }) {
-  const expected = Array.isArray(event) ? event : [event];
-  return expected.some(expectedEvent => current.nextEvents.some(ne => (ne === expectedEvent)))
-    ? <div>{children}</div>
-    : null;
-};
 
 export const Amendment = () => {
   const [current, send] = useMachine(amendmentMachine);
 
   return (
     <div>
-      <div>
-      <HandlesEvent event="REVERT" current={current}>
-        <button onClick={() => send('REVERT')}>
-        REVERT
-        </button>
-      </HandlesEvent>
-      </div>
       <div>
         {
           current.nextEvents.map(evt =>
